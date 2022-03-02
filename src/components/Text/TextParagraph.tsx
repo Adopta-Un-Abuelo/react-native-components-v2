668a9d1a6f1a7e27750fdb706665d5ab42a7f5a4
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-03-02 13:25:56 +0100

    bug in text paragraph

--- src/components/Text/TextParagraph.tsx
+++ src/components/Text/TextParagraph.tsx
@@ -7,20 +7,7 @@ const TextParagraph = (props: Props) =>{
 
     const { style, type, children, weight, fontStyle, ...rest } = props;
 
-    return(type === 'p1' ?
-        <Text
-            style={{
-                fontFamily: weight === 'semibold' ? 'Poppins-SemiBold' : weight === 'medium' ? 'Poppins-Medium' : fontStyle === 'italic' ? 'Poppins-Italic' : 'Poppins-Regular',
-                fontSize: 15,
-                lineHeight: 22,
-                color: Color.text.full,
-                ...style
-            }}
-            {...rest}
-        >
-            {children}
-        </Text>
-    : type === 'p2' ?
+    return(type === 'p2' ?
         <Text
             style={{
                 fontFamily: weight === 'medium' ? 'Poppins-Medium' : fontStyle === 'italic' ? 'Poppins-Italic' : 'Poppins-Regular',
@@ -33,7 +20,20 @@ const TextParagraph = (props: Props) =>{
         >
             {children}
         </Text>
-    : undefined)
+        :
+        <Text
+            style={{
+                fontFamily: weight === 'semibold' ? 'Poppins-SemiBold' : weight === 'medium' ? 'Poppins-Medium' : fontStyle === 'italic' ? 'Poppins-Italic' : 'Poppins-Regular',
+                fontSize: 15,
+                lineHeight: 22,
+                color: Color.text.full,
+                ...style
+            }}
+            {...rest}
+        >
+            {children}
+        </Text>
+    )
 }
 export default TextParagraph;
 export interface Props{

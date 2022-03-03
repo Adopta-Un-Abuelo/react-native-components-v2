37838aa7fdb275f0749d5b04581fe74dd270bc68
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-03-03 12:43:46 +0100

    Progress bar

--- src/components/ProgressBar/VideoProgressBar.tsx
+++ src/components/ProgressBar/VideoProgressBar.tsx
@@ -1,18 +1,25 @@
 import React, { FC, useEffect, useState } from 'react';
 import * as Progress from 'react-native-progress';
 import styled from 'styled-components/native';
-
 import Color from '../../constants/Color';
 import Text from '../Text/Text';
 
 const Container = styled.View`
     flex: 1;
-    flex-direction: column;
 `
 const MarkView = styled.View`
     flex-direction: row;
     margin-top: 10px;
 `
+
+const getSeconds = (secs) => {
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+    var tempSeconds = seconds < 10 ? '0' + seconds : seconds;
+    return (minutes + ':' + tempSeconds);
+}
 
 const ProgressBar: FC<Props> = props =>{
 
@@ -44,13 +51,13 @@ const ProgressBar: FC<Props> = props =>{
                     type='c1'
                     style={{flex: 1, color: Color.text.primary}}
                 >
-                    {new Date(progressLeft * 1000).toISOString().substr(14, 5)}
+                    {getSeconds(progressLeft)}
                 </Text>
                 <Text
                     type='c1'
                     style={{flex: 1, textAlign: 'right', color: Color.text.high}}
                 >
-                    {new Date(props.maxProgress * 1000).toISOString().substr(14, 5)}
+                    {getSeconds(props.maxProgress.toFixed(0))}
                 </Text>
             </MarkView>
         </Container>

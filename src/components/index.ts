6da6194fdcab105f2d1591e2d760a8e29bc53f29
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2022-03-28 17:18:41 +0200

    Pie chart

--- src/components/index.ts
+++ src/components/index.ts
@@ -42,3 +42,4 @@ export { default as ButtonSmall } from './Button/ButtonSmall';
 export { default as CenterModal } from './Modal/CenterModal';
 export { default as CheckboxButton } from './Checkbox/CheckboxButton';
 export { default as SelectVisit } from './Select/SelectVisit';
+export { default as Chart } from './Chart/Chart';

c882a0278d86dd6e574155708691a38fa1c806bc
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-04-12 11:58:54 +0200

    added to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -43,3 +43,4 @@ export { default as CenterModal } from './Modal/CenterModal';
 export { default as CheckboxButton } from './Checkbox/CheckboxButton';
 export { default as SelectVisit } from './Select/SelectVisit';
 export { default as ProgressRing } from './Chart/ProgressRing';
+export { default as AnimatedProgressRing } from './Chart/AnimatedProgressRing';

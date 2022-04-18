d123cd1183ca06ee46cbe8a304f8a5f5e93140c0
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-04-18 12:29:37 +0200

    emojis && pie chart

--- src/components/index.ts
+++ src/components/index.ts
@@ -42,5 +42,5 @@ export { default as ButtonSmall } from './Button/ButtonSmall';
 export { default as CenterModal } from './Modal/CenterModal';
 export { default as CheckboxButton } from './Checkbox/CheckboxButton';
 export { default as SelectVisit } from './Select/SelectVisit';
-export { default as ProgressRing } from './Chart/ProgressRing';
 export { default as AnimatedProgressRing } from './Chart/AnimatedProgressRing';
+export { default as Emoji } from './Image/Emoji';

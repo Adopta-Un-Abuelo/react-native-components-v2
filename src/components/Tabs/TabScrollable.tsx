35a5c7a2e787f2a508ec3ab4a71ea8095edafd65
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-04-21 13:33:20 +0200

    changes

--- src/components/Tabs/TabScrollable.tsx
+++ src/components/Tabs/TabScrollable.tsx
@@ -4,35 +4,32 @@ import styled from 'styled-components/native';
 import Color from '../../constants/Color';
 import Text from '../Text/Text';
 
-const ItemView = styled.Pressable<{enabled: boolean, full?: boolean}>`
+const ItemView = styled.Pressable<{enabled: boolean, full: boolean}>`
     align-items: center;
     padding-bottom: 8px;
     padding-top: 8px;
     padding-left: 16px;
     padding-right: 16px;
-    background-color: ${props => props.enabled ? (props.full === undefined ? Color.background.deepBlue : Color.background.neutral) : undefined};
+    background-color: ${props => props.enabled ? (props.full ? Color.background.deepBlue : Color.background.neutral) : Color.background.soft};
     border-radius: 100px;
 `
 
-const Tabs: FC<Props> = props =>{
+const TabScrollable: FC<Props> = props =>{
 
     const [ currentIndex, setCurrentIndex ] = useState<number>();
     const [ options, setOptions ] = useState<Array<{date: Date, title: string, enabled: boolean}>>([]);
 
     useEffect(() =>{
         setOptions(props.options);
+        setCurrentIndex(props.options.length-1);
     }, []);
-
-    useEffect(() =>{
-        setCurrentIndex(props.currentIndex);
-    }, [props.currentIndex]);
 
     const getItemLayout = (data, index) => {
         return { length: 48, offset: 48 * index, index }
     }
 
     const onTabPress = (item, index) => {
-        const newOptions = props.options.map((e, index) => {
+        const newOptions = options.map((e, index) => {
             if(item.title === e.title) {
                 return {
                     ...e,
@@ -46,6 +43,7 @@ const Tabs: FC<Props> = props =>{
             }
         });
         setOptions(newOptions);
+        setCurrentIndex(index);
         props.onTabPress && props.onTabPress(item, index);
     }
 
@@ -55,13 +53,14 @@ const Tabs: FC<Props> = props =>{
                 enabled={item.enabled}
                 full={item.date === undefined}
                 onPress={() => onTabPress(item, index)}
+                style={props.tabStyle}
             >
                 <Text
                     type='p2'
                     weight='medium'
                     style={{color: item.enabled ? (item.date === undefined ? Color.text.white : Color.text.full) : Color.text.high}}
                 >
-                    {item.month}
+                    {item.title}
                 </Text>
             </ItemView>
         )
@@ -82,7 +81,7 @@ const Tabs: FC<Props> = props =>{
     )
 }
 
-export default Tabs;
+export default TabScrollable;
 export interface Props{
     style?: Object,
     tabStyle?: Object,
@@ -91,6 +90,5 @@ export interface Props{
         title: string,
         enabled: boolean
     }>,
-    currentIndex: number,
     onTabPress: Function
 }

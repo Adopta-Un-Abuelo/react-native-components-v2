bdb238121f9df73e1f1c9521880f74a5b99f88a8
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-06-23 13:27:27 +0200

    Gradient
    - Button
    - ProgressBar

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ import LottieView from 'lottie-react-native';
 import { GestureResponderEvent, PressableProps, ViewStyle, TextStyle } from 'react-native';
 import Color from '../../constants/Color';
 import Text from '../Text/Text';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Container = styled.Pressable<{type?: 'line' | 'fill', color?: string, size?: 'small' | 'big'}>`
     flex-direction: row;
@@ -17,6 +18,14 @@ const Container = styled.Pressable<{type?: 'line' | 'fill', color?: string, size
     padding-left: 24px;
     padding-right: 24px;
     opacity: ${props => props.disabled ? 0.48 : 1};
+`
+const Gradient = styled(LinearGradient)`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    border-radius: 12px;
 `
 
 const Button = (props: Props) =>{
@@ -44,23 +53,30 @@ const Button = (props: Props) =>{
                 />
             :
                 <>
-                {props.icon &&
-                    <props.icon style={{marginRight: 16}} color={props.color ? props.color : Color.text.full}/>
-                }
-                <Text
-                    type='b1'
-                    weight='semibold'
-                    style={{
-                        textAlign: 'center',
-                        color: props.color ? props.color : Color.text.white,
-                        ...props.textStyle
-                    }}
-                    numberOfLines={1}
-                    ellipsizeMode={'tail'}
-                    adjustsFontSizeToFit={true}
-                >
-                    {props.title}
-                </Text>
+                    {props.gradient &&
+                        <Gradient
+                            colors={[props.gradient.colorStart, props.gradient.colorEnd]}
+                            start={{x: 0, y: 0}} 
+                            end={{x: 1, y: 1}}
+                        />
+                    }
+                    {props.icon &&
+                        <props.icon style={{marginRight: 16}} color={props.color ? props.color : Color.text.full}/>
+                    }
+                    <Text
+                        type='b1'
+                        weight='semibold'
+                        style={{
+                            textAlign: 'center',
+                            color: props.color ? props.color : Color.text.white,
+                            ...props.textStyle
+                        }}
+                        numberOfLines={1}
+                        ellipsizeMode={'tail'}
+                        adjustsFontSizeToFit={true}
+                    >
+                        {props.title}
+                    </Text>
                 </>
             }
         </Container>
@@ -75,5 +91,9 @@ export interface Props extends PressableProps{
     color?: string,
     icon?: any,
     type?: 'fill' | 'line',
-    size?: 'big' | 'small'
+    size?: 'big' | 'small',
+    gradient?: {
+        colorStart: string,
+        colorEnd: string
+    }
 }
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect, useState } from 'react';
 import * as Progress from 'react-native-progress';
 import styled from 'styled-components/native';
+import SimpleGradientProgressbarView from 'react-native-simple-gradient-progressbar-view';
+import { StyleSheet } from 'react-native'
 
 const ProgressView = styled.View`
     flex: 1;
@@ -16,19 +18,38 @@ const ProgressBar: FC<Props> = props =>{
     },[props.progress, props.maxProgress]);
 
     return(
-        <ProgressView
-            style={props.style}
-        >
-            <Progress.Bar
-                width={null}
-                progress={barProgress}
-                height={props.height ? props.height : 6}
-                borderRadius={100}
-                borderWidth={0}
-                unfilledColor={props.colorBarTotal}
-                color={props.colorBarLeft}
-            />
-        </ProgressView>
+        <>
+            {props.gradient ? 
+                <SimpleGradientProgressbarView
+                    style={StyleSheet.flatten([
+                        props.style,
+                        {
+                            backgroundColor: props.colorBarTotal,
+                            height: props.height ? props.height : 12,
+                            borderRadius: 6,
+                        }])}
+                    fromColor={props.colorBarLeft}
+                    toColor={props.colorBarLeftGradient}
+                    progress={barProgress}
+                    maskedCorners={[1, 1, 1, 1]}
+                    cornerRadius={6}
+                />
+            :
+                <ProgressView
+                    style={props.style}
+                >
+                    <Progress.Bar
+                        width={null}
+                        progress={barProgress}
+                        height={props.height ? props.height : 6}
+                        borderRadius={100}
+                        borderWidth={0}
+                        unfilledColor={props.colorBarTotal}
+                        color={props.colorBarLeft}
+                    />
+                </ProgressView>
+            }
+        </>
     )
 }
 export default ProgressBar;
@@ -38,5 +59,7 @@ export interface Props{
     progress: number,
     maxProgress: number,
     colorBarLeft?: any,
-    colorBarTotal?: any
+    colorBarLeftGradient?: any,
+    colorBarTotal?: any,
+    gradient?: boolean
 }

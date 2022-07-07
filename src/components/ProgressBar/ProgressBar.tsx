be80e51b2b3923e2aa6dcf6c89057e74032231d9
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-07-07 12:36:00 +0200

    upgraded stripe version
    changed progress bar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,12 +1,6 @@
 import React, { FC, useEffect, useState } from 'react';
-import * as Progress from 'react-native-progress';
-import styled from 'styled-components/native';
 import SimpleGradientProgressbarView from 'react-native-simple-gradient-progressbar-view';
 import { StyleSheet } from 'react-native'
-
-const ProgressView = styled.View`
-    flex: 1;
-`
 
 const ProgressBar: FC<Props> = props =>{
 
@@ -18,38 +12,20 @@ const ProgressBar: FC<Props> = props =>{
     },[props.progress, props.maxProgress]);
 
     return(
-        <>
-            {props.gradient ? 
-                <SimpleGradientProgressbarView
-                    style={StyleSheet.flatten([
-                        props.style,
-                        {
-                            backgroundColor: props.colorBarTotal,
-                            height: props.height ? props.height : 12,
-                            borderRadius: 6,
-                        }])}
-                    fromColor={props.colorBarLeft}
-                    toColor={props.colorBarLeftGradient}
-                    progress={barProgress}
-                    maskedCorners={[1, 1, 1, 1]}
-                    cornerRadius={6}
-                />
-            :
-                <ProgressView
-                    style={props.style}
-                >
-                    <Progress.Bar
-                        width={null}
-                        progress={barProgress}
-                        height={props.height ? props.height : 6}
-                        borderRadius={100}
-                        borderWidth={0}
-                        unfilledColor={props.colorBarTotal}
-                        color={props.colorBarLeft}
-                    />
-                </ProgressView>
-            }
-        </>
+        <SimpleGradientProgressbarView
+            style={StyleSheet.flatten([
+                props.style,
+                {
+                    backgroundColor: props.colorBarTotal,
+                    height: props.height ? props.height : 12,
+                    borderRadius: 6,
+                }])}
+            fromColor={props.colorBarLeft}
+            toColor={props.colorBarLeftGradient ? props.colorBarLeftGradient : props.colorBarLeft}
+            progress={barProgress}
+            maskedCorners={[1, 1, 1, 1]}
+            cornerRadius={6}
+        />
     )
 }
 export default ProgressBar;
@@ -60,6 +36,5 @@ export interface Props{
     maxProgress: number,
     colorBarLeft?: any,
     colorBarLeftGradient?: any,
-    colorBarTotal?: any,
-    gradient?: boolean
+    colorBarTotal?: any
 }

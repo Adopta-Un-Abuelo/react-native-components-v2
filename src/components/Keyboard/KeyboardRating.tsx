4556978f186204b8123c3940167c866167027355
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2022-11-17 15:57:33 +0100

    Keyboard Rating

--- src/components/Keyboard/KeyboardRating.tsx
+++ src/components/Keyboard/KeyboardRating.tsx
@@ -1,97 +1,80 @@
 import React, { FC, useState } from 'react';
-import { View, TouchableHighlight, StyleSheet, ViewStyle } from 'react-native';
+import { ViewStyle } from 'react-native';
 import Text from '../Text/Text';
 import Color from '../../constants/Color';
+import styled from 'styled-components/native';
 
-export const keyStyle = (isPressing) => StyleSheet.create({
-    wrapperOut: {
-        flexDirection: 'row'
-    },
-    main: {
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    row: {
-        flexDirection: 'row'
-    },
-    wrapperIn: {
-        height: 48,
-        width: 48,
-        borderRadius: 48/2,
-        margin: 8,
-        borderColor: isPressing ? Color.line.primary : Color.line.soft,
-        borderWidth: isPressing ? 2 : 1
-    },
-    bd: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
+const WrapperOut = styled.View`
+    flex-direction: row;
+`
+const Main = styled.View`
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`
+const WrapperIn = styled.TouchableHighlight<{isPressing: boolean}>`
+    height: 48px;
+    width: 48px;
+    border-radius: 100px;
+    border-color: ${props => props.isPressing ? Color.line.primary : Color.line.soft};
+    border-width: ${props => props.isPressing ? '2px' : '1px'};
+    background-color: ${props => props.isPressing ? Color.background.primaryLow : 'white'};
+`
+const BD = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`
 
 const numberKeys = [
-    [
-        { mainText: '1' },
-        { mainText: '2' },
-        { mainText: '3' },
-        { mainText: '4' },
-        { mainText: '5' }
-    ]
+    { mainText: '1' },
+    { mainText: '2' },
+    { mainText: '3' },
+    { mainText: '4' },
+    { mainText: '5' }
 ];
 
 const KeyboardRating: FC<Props> = props =>{
 
-    const [ isPressing, setIsPressing ] = useState(false);
-    // const [ index, setIndex ] = useState(0);
+    const [ selectedOption, setSelectedOption ] = useState<any>(undefined);
 
     const _onPress = key =>{
         if (key === '') {
             return;
         } else {
+            setSelectedOption(key);
             props.onKeyPress && props.onKeyPress(key);
         }
     }
-    
-    const _renderKey = (key, index) =>{
-        return (
-            <TouchableHighlight
-                key={index}
-                activeOpacity={1}
-                style={{...keyStyle(isPressing).wrapperIn, ...props.style}}
-                // onShowUnderlay={() => {setIsPressing(true); setIndex(index)}}
-                // onHideUnderlay={() => {setIsPressing(false); setIndex(0)}}
-                underlayColor={Color.status.primary.softDefault}
-                onPress={() => _onPress(key.mainText)}
-            >
-                <View style={[keyStyle(isPressing).bd]}>
-                    <Text 
-                        type='p1'
-                        weight='semibold'
-                        style={{color: Color.text.high}}
-                    >
-                        {key.mainText}
-                    </Text>
-                </View>
-            </TouchableHighlight>
-        );
-    }
-
-    const _renderNumberKeys = () =>{
-        return numberKeys.map((group, groupIndex) => {
-            return (
-                <View key={groupIndex} style={keyStyle(isPressing).row}>
-                    {group.map(_renderKey.bind(this))}
-                </View>
-            );
-        });
-    }
 
     return (
-        <View style={keyStyle(isPressing).wrapperOut}>
-            <View style={keyStyle(isPressing).main}>
-                {_renderNumberKeys()}
-            </View>
-        </View>
+        <WrapperOut>
+            <Main>
+                {numberKeys.map((item, index) =>(
+                    <WrapperIn
+                        key={index}
+                        activeOpacity={1}
+                        style={props.style}
+                        isPressing={item.mainText === selectedOption}
+                        // onShowUnderlay={() => {setIsPressing(true); setIndex(index)}}
+                        // onHideUnderlay={() => {setIsPressing(false); setIndex(0)}}
+                        underlayColor={Color.status.primary.softDefault}
+                        onPress={() => _onPress(item.mainText)}
+                    >
+                        <BD>
+                            <Text 
+                                type='p1'
+                                weight='semibold'
+                                style={{color: item.mainText === selectedOption ? Color.text.full : Color.text.high}}
+                            >
+                                {item.mainText}
+                            </Text>
+                        </BD>
+                    </WrapperIn>
+                ))}
+            </Main>
+        </WrapperOut>
     );
 }
 
